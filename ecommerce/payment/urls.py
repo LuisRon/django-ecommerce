0b6247db5d22027a628d8aeaed153eb64b6757ee
Luis Ron <luisron2007@gmail.com>
2024-10-27 12:02:33 +0100

    finish shipping form

--- ecommerce/payment/urls.py
+++ ecommerce/payment/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 
+    path('checkout', views.checkout, name='checkout'),
     path('payment-success', views.payment_success, name='payment-success'),
     path('payment-failed', views.payment_failed, name='payment-failed'),
 
--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def checkout(request):
+    return render(request, 'payment/checkout.html')
+
+
 def payment_success(request):
 
     return render(request, 'payment/payment-success.html')
